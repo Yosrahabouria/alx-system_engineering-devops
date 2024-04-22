790d346a9a2600958506887f8093adf07b863d61
Yosrahabouria <MY_NAME@example.com>
2024-04-22 10:23:46 -0700

    export

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Python script to export data in the CSV format"""
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    rq = requests.get(url)
+    if rq.status_code == 200:
+        user_name = rq.json().get("username")
+        url2 = 'https://jsonplaceholder.typicode.com/todos'
+        rq2 = requests.get(url2)
+        filename = sys.argv[1] + '.csv'
+        with open(filename, 'w') as f:
+            wr = csv.writer(f, quoting=csv.QUOTE_ALL, delimiter=',')
+            for item in rq2.json():
+                if item.get("userId") == int(sys.argv[1]):
+                    line = [item.get("userId"),
+                            username,
+                            str(item.get("completed")),
+                            item.get('title')]
+                    wr.writerow(line)
