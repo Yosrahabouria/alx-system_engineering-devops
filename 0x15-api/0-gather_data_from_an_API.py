37b048cf1ca14ee61b2b3c0041494ab2f440de18
Yosrahabouria <MY_NAME@example.com>
2024-04-22 10:25:50 -0700

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,14 @@ import sys
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    r = requests.get(url)
-    name = r.json().get("name")
+    rq = requests.get(url)
+    name = rq.json().get("name")
     url2 = 'https://jsonplaceholder.typicode.com/todos'
-    r2 = requests.get(url2)
+    rq2 = requests.get(url2)
     task_names = []
     tasks = 0
     completed = 0
-    for item in r2.json():
+    for item in rq2.json():
         if item.get("userId") == int(sys.argv[1]):
             tasks += 1
             if item.get("completed") is True:
@@ -23,5 +23,5 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".format(name,
                                                           completed,
                                                           tasks))
-    for i in task_names:
-        print("\t {}".format(i))
+    for j in task_names:
+        print("\t {}".format(j))
