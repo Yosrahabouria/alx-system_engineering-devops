4e424da099709cbf54b6f0aeaf948c04290700de
Yosrahabouria <yosrahabouria@gmail.com>
2024-05-08 05:19:04 -0700

    hot lisrs

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+returns a list containing the titles of all hot articles
+"""
+import requests
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'after': after}
+    res = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if res.status_code == 200:
+        after_data = res.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = res.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
