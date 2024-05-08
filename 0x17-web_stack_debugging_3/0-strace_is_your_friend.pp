d6b9fec3e90b1e7b91198e2e472cb452c41c5201
Yosrahabouria <yosrahabouria@gmail.com>
2024-05-08 06:10:17 -0700

    fix mermessions

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,16 @@
+$uploads_dir = '/var/www/html/wp-content/uploads'
+
+file { $uploads_dir:
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+exec { 'fix-wp-uploads-permissions':
+  command => "chmod -R u=rwX,go=rX ${uploads_dir}",
+  path    => '/bin:/usr/bin',
+  onlyif  => "test -d ${uploads_dir}",
+  require => File[$uploads_dir],
+}
